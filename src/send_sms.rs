f0ec135e4c9cc6b31877ae08bcb82173222ac491
amigin <amigin@gmail.com>
2025-09-25 20:22:06 +0300

    Fixes with topic-name

--- src/send_sms.rs
+++ src/send_sms.rs
@@ -2,7 +2,7 @@ use service_sdk::my_service_bus;
 use service_sdk::my_service_bus::macros::my_sb_entity_protobuf_model;
 
 #[derive(Clone, PartialEq, ::prost::Message)]
-#[my_sb_entity_protobuf_model(topic_id = "exec-crypto-deposit")]
+#[my_sb_entity_protobuf_model(topic_id = "send-sms")]
 pub struct SendSmsMySbContract {
     #[prost(string, tag = "1")]
     pub inventory_id: String,
