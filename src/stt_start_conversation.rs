c7fd0135233dd721d153b8f4cd5fbfe05e36b239
amigin <amigin@gmail.com>
2025-11-26 20:04:21 +0200

    Naming Fixes

--- src/stt_start_conversation.rs
+++ src/stt_start_conversation.rs
@@ -3,7 +3,7 @@ use service_sdk::my_service_bus::macros::my_sb_entity_protobuf_model;
 
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[my_sb_entity_protobuf_model(topic_id = "stt-start-conversation")]
-pub struct SttStartConversation {
+pub struct SttStartConversationMySbContract {
     #[prost(string, tag = "1")]
     pub session_id: String,
     #[prost(string, tag = "2")]
